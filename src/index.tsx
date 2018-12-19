7b109bf8e2e09ea2828d501adf7289a7b4d7da2c
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-18 21:49:57 -0400

    Added Tailwind CSS an styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import tw from "tailwind.macro";
+import * as serviceWorker from "./serviceWorker";
+
+const Button = styled.button`
+  ${tw`font-mono text-sm text-red hover:text-blue`};
+  background-color: #ffffff;
+  padding: 25px;
+`;
 
 ReactDOM.render(
     <div>
-        <h1>Hello world</h1>
+        <Button>Hello world</Button>
     </div>
-    , document.getElementById('root'));
+    , document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
