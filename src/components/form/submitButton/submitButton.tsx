3a436779a0513ac6f6aa000c858de48396825e20
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-24 13:23:19 -0400

    SubmitButton component

---
+++ src/components/form/submitButton/submitButton.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+export interface ISubmitButton {
+  disabled?: boolean;
+}
+
+const SubmitButton = styled.button.attrs({
+  type: 'submit'
+})<ISubmitButton>`
+  ${tw`px-4 py-2 font-bold rounded`};
+  
+  ${props => props.disabled ?
+    tw`bg-grey-darker text-white cursor-not-allowed`
+    :
+    tw`bg-primary text-white hover:bg-blue-darker`
+  }
+`;
+
+export default SubmitButton;
