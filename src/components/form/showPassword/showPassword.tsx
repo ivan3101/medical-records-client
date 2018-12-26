558693ae9aa7325fa492f5ef874f79f7e1d0b184
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-26 17:08:41 -0400

    Show Password component

---
+++ src/components/form/showPassword/showPassword.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent, MouseEventHandler } from "react";
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+export interface IShowPassword {
+  onClick: MouseEventHandler<HTMLInputElement>;
+}
+
+const StyledLabel = styled.label`
+  ${tw`inline-flex items-center text-sm font-normal my-2 lg:my-0`};  
+`;
+
+const StyledCheckbox = styled.input`
+  ${tw`cursor-pointer`};
+`;
+
+const ShowPassword: FunctionComponent<IShowPassword> = ({onClick: handleClick}) => {
+  return (
+    <StyledLabel>
+      <StyledCheckbox
+        type="checkbox"
+        defaultChecked={false}
+        onClick={handleClick}
+      />
+
+      {" "} Mostrar contraseña
+
+    </StyledLabel>
+  );
+};
+
+export default ShowPassword;
