e1e0bc03742d1094e03fc382b671d1d9e85de1c4
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-26 00:04:14 -0400

    Login Card component

---
+++ src/views/login/components/loginCard/loginCard.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+import Card from "../../../../components/card/card";
+
+const LoginCard = styled(Card)`
+  ${tw`w-full`};
+`;
+
+export default LoginCard;
