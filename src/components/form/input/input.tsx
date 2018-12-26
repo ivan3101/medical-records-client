5dd3aa40f8264351d3d3e177e236f83590691a30
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-26 00:03:41 -0400

    Input states added

--- src/components/form/input/input.tsx
+++ src/components/form/input/input.tsx
@@ -1,9 +1,21 @@
-import { Field } from "formik";
+import { Field, FieldProps } from "formik";
 import styled from "styled-components/macro";
 import tw from "tailwind.macro";
 
-const Input = styled(Field)`
-  ${tw`block w-full rounded px-2 py-1`};
+export interface IInput extends FieldProps {
+  isinvalid: number;
+}
+
+const Input = styled(Field).attrs({
+  type: 'text'
+})<IInput>`
+  ${tw`block w-full rounded px-2 py-1 bg-white border-2 text-black`};
+  
+  :focus {
+    box-shadow: 0 0 6px 0 ${props => props.isinvalid ? '#cc1f1a' : '#3490dc'};
+  }
+  
+  ${props => props.isinvalid ? tw`border-red-dark focus:border-red-dark` : tw`border-grey-dark focus:border-blue`};
 `;
 
 export default Input;
