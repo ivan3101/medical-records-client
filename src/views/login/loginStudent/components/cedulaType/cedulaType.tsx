8f0352edac3a62ec023252a2923a2ad42fa22ed7
Ivan De Menezes <ivandmenezes@gmail.com>
2018-12-26 01:02:11 -0400

    Cedula type component

---
+++ src/views/login/loginStudent/components/cedulaType/cedulaType.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+import Input from "../../../../../components/form/input/input";
+
+const CedulaType = styled(Input)`
+  ${tw`w-full mb-3 lg:w-16 lg:mr-3 lg:mb-0`}
+`;
+
+export default CedulaType;
