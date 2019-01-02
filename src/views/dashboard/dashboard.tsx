0b56b00e14a8dbe981bc8bf47ea0f2ca7ec593a2
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-01 22:32:46 -0400

    Sidebar test

--- src/views/dashboard/dashboard.tsx
+++ src/views/dashboard/dashboard.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import patientsIcon from "../../assets/icons/patients.svg";
+import DashboardLayout from "../../components/dashboardLayout/dashboardLayout";
+import Sidebar from "../../components/sidebar/sidebar";
+import SidebarItem from "../../components/sidebar/sidebarItem/sidebarItem";
+import Icon from "../../components/sidebar/sidebarItem/sidebarLink/icon/icon";
 
 const Dashboard = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <React.Fragment>
+      <Sidebar>
+        <SidebarItem to={"/"}>
+          <Icon src={patientsIcon} />
+          Paciente
+        </SidebarItem>
+      </Sidebar>
+
+      <DashboardLayout>hello world</DashboardLayout>
+    </React.Fragment>
   );
 };
 
