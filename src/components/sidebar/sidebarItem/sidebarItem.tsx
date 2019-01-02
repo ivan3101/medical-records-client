231296e64006e8d5ad103cd1c6bc9e9bfd4d8e4a
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-02 00:56:41 -0400

    Active sidebar link

--- src/components/sidebar/sidebarItem/sidebarItem.tsx
+++ src/components/sidebar/sidebarItem/sidebarItem.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { RouterLinkProps } from "../../link/routerLink/routerLink";
-import SidebarLink from "./sidebarLink/sidebarLink";
+import SidebarLink, { sidebarActiveClass } from "./sidebarLink/sidebarLink";
 
 const SidebarItem: FunctionComponent<RouterLinkProps> = ({
   children,
@@ -9,7 +9,9 @@ const SidebarItem: FunctionComponent<RouterLinkProps> = ({
 }) => {
   return (
     <li className={className}>
-      <SidebarLink to={to}>{children}</SidebarLink>
+      <SidebarLink to={to} activeClassName={sidebarActiveClass}>
+        {children}
+      </SidebarLink>
     </li>
   );
 };
--- src/components/sidebar/sidebarItem/sidebarLink/sidebarLink.tsx
+++ src/components/sidebar/sidebarItem/sidebarLink/sidebarLink.tsx
@@ -2,8 +2,20 @@ import styled from "styled-components/macro";
 import tw from "tailwind.macro";
 import RouterLink from "../../../link/routerLink/routerLink";
 
-const SidebarLink = styled(RouterLink)`
+export const sidebarActiveClass = "active";
+
+export interface ISidebarLink {
+  activeClassName: "active";
+}
+
+const SidebarLink = styled(RouterLink).attrs({
+  activeClassName: sidebarActiveClass
+})<ISidebarLink>`
   ${tw`p-4 text-white no-underline font-normal mb-4 h-auto w-full rounded mt-0 inline-flex items-center hover:bg-primary hover:text-white`};
+
+  &.${sidebarActiveClass} {
+    ${tw`bg-primary font-bold`};
+  }
 `;
 
 export default SidebarLink;
