ef1d0e23bda91679e64d7082527a2a5f1b720307
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:39:51 -0400

    flex grid

---
+++ src/components/flexGrid/flexGrid.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+const FlexGrid = styled.div`
+  ${tw`flex flex-row flex-wrap justify-around mt-3`};
+`;
+
+export default FlexGrid;
