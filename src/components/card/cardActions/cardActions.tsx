2045b6e1538d1031e5eda09d080672f4e7e72dd6
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:54:29 -0400

    Card Actions component

---
+++ src/components/card/cardActions/cardActions.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+const CardActions = styled.div`
+  ${tw`flex flex-row max-w-full items-center px-6 py-3`};
+`;
+
+export default CardActions;
