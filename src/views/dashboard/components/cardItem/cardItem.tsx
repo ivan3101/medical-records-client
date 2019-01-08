6aaf10ac899d8a12de584f9999e0b6fe2de99eaa
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:35:10 -0400

    Flex card item

---
+++ src/views/dashboard/components/cardItem/cardItem.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import Card from "../../../../components/card/card";
+
+const CardItem = styled(Card)`
+  flex: 0 1 20%;
+`;
+
+export default CardItem;
