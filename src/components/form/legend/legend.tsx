fbc80c653852f91b1fa77a10fcafdeb81f629b62
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:32:42 -0400

    Legend component

---
+++ src/components/form/legend/legend.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+const Legend = styled.legend`
+  ${tw`bg-grey-light rounded text-sm p-3`};
+`;
+
+export default Legend;
