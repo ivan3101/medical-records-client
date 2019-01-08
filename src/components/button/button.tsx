7847a90d5dfc4c1875c6d5a7a3a5c423d01e941a
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:31:58 -0400

    Button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+export interface IButton {
+  disabled?: boolean;
+}
+
+const Button = styled.button.attrs({
+  type: "button"
+})<IButton>`
+  ${tw`px-4 py-2 font-bold rounded w-auto text-sm`};
+
+  ${props =>
+    props.disabled
+      ? tw`bg-grey-darker text-white cursor-not-allowed`
+      : tw`bg-primary text-white hover:bg-blue-darker`}
+`;
+
+export default Button;
