eda48cf26c867dd0ed2dfa628cc4c4984b82458e
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:54:00 -0400

    fix style

--- src/views/dashboard/components/cardItem/cardItem.tsx
+++ src/views/dashboard/components/cardItem/cardItem.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components/macro";
+import tw from "tailwind.macro";
 import Card from "../../../../components/card/card";
 
 const CardItem = styled(Card)`
+  ${tw`mb-4`};
+
   flex: 0 1 20%;
 `;
 
