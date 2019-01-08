d1508f464fde638420a2c7c70862f0d26e032f4d
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-08 02:32:06 -0400

    Card Img component

---
+++ src/components/card/cardImg/cardImg.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+const CardImg = styled.img`
+  ${tw`block my-0 mx-auto h-24 w-24 mb-3`}
+`;
+
+export default CardImg;
