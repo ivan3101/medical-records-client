0bc621d3a6bacb72d35290aa5fd2d97396605598
Ivan De Menezes <ivandmenezes@gmail.com>
2019-01-11 02:59:52 -0400

    Container component

---
+++ src/components/container/container.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components/macro";
+import tw from "tailwind.macro";
+
+const Container = styled.div`
+  ${tw`my-0 mx-auto w-full max-w-md h-auto bg-white rounded shadow-md px-12 py-8`};
+`;
+
+export default Container;
